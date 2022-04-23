fceea96b239579b560a97873fb54cbac93a2c0d6
Ioannis Eleftheriadis <mlbcraven@gmail.com>
2022-04-23 21:59:19 +0300

    results

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let showbtn1 = document.getElementById("Add")
+let showresults = document.getElementById("results")
 console.log(showbtn1)
 let btn1 = 0
 function incrementbtn1() {
@@ -9,8 +10,13 @@ function incrementbtn2() {
     btn1 = btn1 + 2
     showbtn1.innerText = btn1
 }
+
 function save() {
-    btn1 = 0
+    let entry = btn1
     showbtn1.innerText = btn1 
+    showresults.innerText += entry + " - "
+    btn1 = 0
+
+   
 
 }
